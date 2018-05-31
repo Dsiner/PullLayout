8e02a27093e9d78409200dfe06b1e8e48430c796
D <s90789@outlook.com>
2018-05-31 16:07:44 +0800

    Update gradle

--- app/src/androidTest/java/com/d/refreshlayout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/d/refreshlayout/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.d.xrefreshlayout;
+package com.d.refreshlayout;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.d.xrefreshlayout", appContext.getPackageName());
+        assertEquals("com.d.refreshlayout", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/d/refreshlayout/MainActivity.java
+++ app/src/main/java/com/d/refreshlayout/MainActivity.java
@@ -1,4 +1,4 @@
-package com.d.xrefreshlayout;
+package com.d.refreshlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
